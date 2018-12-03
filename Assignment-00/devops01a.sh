7ed7d3f3ae1d18e2cc60790fc576eb444cee9d33
Gary Sanders <xpkidd@yahoo.com>
2018-12-02 20:24:18 -0800

    Adding number of vol, size of vol, free space of vol

---
+++ Assignment-00/devops01a.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+clear
+df -h
+echo
+echo
+#echo "Computer cpus/core information"
+#echo "------------------------------"
+#wmic cpu get caption, deviceid, name, numberofcores, maxclockspeed, status
+#echo
+#echo
+#echo "Computer RAM"
+##echo "------------"
+#free -h
+#echo
+#echo
+#echo "Computer IP address and MAC address"
+#echo "-----------------------------------"
+#ipconfig /all
+
+
+
