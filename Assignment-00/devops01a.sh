317d9210b2d610cf8d636f95d1d1459f8b628a4d
Gary Sanders <xpkidd@yahoo.com>
2018-12-03 04:10:21 -0800

    Adding mac address and ip address of machine

--- Assignment-00/devops01a.sh
+++ Assignment-00/devops01a.sh
@@ -9,14 +9,11 @@ echo -ne "Number of CPU: ";grep -c ^processor /proc/cpuinfo
 inxi -C
 echo
 echo
-#echo "Computer RAM"
-##echo "------------"
 free -h
-#echo
-#echo
-#echo "Computer IP address and MAC address"
-#echo "-----------------------------------"
-#ipconfig /all
-
+echo
+echo
+ifconfig 
+echo
+echo
 
 
